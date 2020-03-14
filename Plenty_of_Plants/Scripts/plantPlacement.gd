13a8f6891449a1679cb09b644fb9ba9efc857ce7
Gad3lan <alan.adamiak@gmail.com>
2020-03-14 11:52:35 +0100

    Suppression espace

---
+++ Plenty_of_Plants/Scripts/plantPlacement.gd
@@ -0,0 +1,15 @@
+extends Node
+
+export (int) var xTilesNumber
+export (int) var yTilesNumber
+var tileSize = Vector2()
+
+func _ready():
+	tileSize = Vector2(get_viewport().size.x / xTilesNumber, get_viewport().size.y / yTilesNumber)
+
+func _process(delta):
+	var mousePos = get_viewport().get_mouse_position()
+	var xTile = int(round(mousePos.x / tileSize.x))
+	var yTile = int(round(mousePos.y / tileSize.y))
+	#print(xTile, " ", yTile)
+	print(get_viewport().size)
