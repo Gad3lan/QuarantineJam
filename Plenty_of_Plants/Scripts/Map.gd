c08dfa33b60342855f016881b31685419c8bb8f8
pierre <pierre.garcia@u-psud.fr>
2020-03-14 18:49:55 +0100

    Beginning map generation

--- Plenty_of_Plants/Scripts/Map.gd
+++ Plenty_of_Plants/Scripts/Map.gd
@@ -1,13 +1,51 @@
 extends Node2D
 
+var allTiles : Array
+export var tileCountA : int = 30 # sur le vecteur (1,1)
+export var tileCountB : int = 30 # sur le vecteur (-1,1)
+export var firstTilePos = Vector2(-175.0,-85)
 
+
+var tilesWithBuilding : Array
+var tilesWithPlants : Array
+
+
+export var tileSpacing = Vector2(360.0,180.0)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
+
+func initTiles():
+	var rowB : Array = []
+	var idx = 0
+	for b in range(tileCountA):
+		rowB = []
+		for a in range(tileCountB):
+			rowB.push_back(null)
+		allTiles.push_back(rowB.duplicate())
+		rowB.empty()
+	for tile in get_tree().get_nodes_in_group("Tiles"):
+		var arrayPosition = Vector2(0,0)
+		arrayPosition.x = floor((tile.position.x - firstTilePos.x)/(tileSpacing.x))
+		arrayPosition.y = floor((tile.position.y - firstTilePos.y)/(tileSpacing.y))
+		#on passe dans un repère basé sur la taille des losanges
+		for rows in allTiles:
+			for t in rows:
+				if t == tile:
+					print("error, already exists")
+					print(arrayPosition.x,", ",arrayPosition.y)
+		
+		allTiles[(arrayPosition.x+arrayPosition.y)*0.5][(-arrayPosition.x+arrayPosition.y)*0.5] = tile
+		#on tourne de 45 degrees
+	print(allTiles)
+	assert(allTiles.size() == tileCountA)
+	assert(allTiles[-1].size() == tileCountB)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initTiles()
 	pass # Replace with function body.
 
 
--- Plenty_of_Plants/Scripts/Tile.gd
+++ Plenty_of_Plants/Scripts/Tile.gd
@@ -1,6 +1,27 @@
 extends Area2D
 
+export var startTile = false
 
+var hasPlant = false
+var hasBuilding : bool = false
+
+
+onready var plant : Sprite = get_node("Plant")
+onready var building : Sprite = get_node("Building")
+
+var canPlacePlant = false
+
+
+func toggle_plantability(toggle):
+	canPlacePlant = toggle
+
+func plant(toPlant):
+	hasPlant = true
+	plant.texture = toPlant
+
+func _on_Tile_input_event (viewport, event, shape_idx):
+	
+	pass
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,6 +29,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_to_group("Tiles")
 	pass # Replace with function body.
 
 
