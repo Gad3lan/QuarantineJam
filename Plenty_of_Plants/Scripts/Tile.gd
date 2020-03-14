53665788cbdccf0ef311e53fdd04d44ff8d2f9e3
Gad3lan <alan.adamiak@gmail.com>
2020-03-14 18:48:17 +0100

    Outline shader sur les tuiles

--- Plenty_of_Plants/Scripts/Tile.gd
+++ Plenty_of_Plants/Scripts/Tile.gd
@@ -4,13 +4,26 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var hoovered = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sprite.material.set_shader_param("width", 0.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if (self.hoovered):
+		#$Sprite.material.set_shader_param("width", 4.0)
+		$Sprite.material.set_shader_param("outline_color", Color(0.5, 0.6, 0.8, 1.0))
+	#else:
+		#self.get_child(0).material.set_shader_param("width", 0.0)
+		#$Sprite.material.set_shader_param("width", 0.0)
+
+func _on_Tile_mouse_entered():
+	self.hoovered = true
+	$Sprite.material.set_shader_param("width", 4.0)
+
+func _on_Tile_mouse_exited():
+	self.hoovered = false
+	$Sprite.material.set_shader_param("width", 0.0)
