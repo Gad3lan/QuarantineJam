31a87747ff9bb60f957011f8e9935f582fc2ff7f
adblackx <adblackx@gmail.com>
2020-03-14 19:40:57 +0100

    BioMasse2

--- Plenty_of_Plants/Scripts/GetBioMasse.gd
+++ Plenty_of_Plants/Scripts/GetBioMasse.gd
@@ -1,24 +1,35 @@
-extends Sprite
+extends Area2D
+
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+var add_Value = 0
+var timeToDelete = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Timer.set_one_shot(true)
+	$Timer.set_wait_time(timeToDelete)
+	$Timer.connect("timeout", self, "_timer_callback")
+	$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	print($Timer.time_left/timeToDelete)
+	# juste un test en cartion pour rendre la piece transparente avant qu'elle disparaisse
+	var ratio = 0.5+$Timer.time_left/timeToDelete
+	$Sprite.modulate = Color(1,1,1,ratio)
+	if $Timer.time_left == 0 :
+		self.queue_free()
 
 func _input(event):
-	if event is InputEventMouseButton:
-		#var mousePos = get_viewport().get_mouse_position()
-		get_rect().has_point(to_local(event.position))
-		if get_rect().has_point(to_local(event.position)) == true:
-			visible = false
-		#print(get_rect().has_point(to_local(event.position)))
+
+	pass
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	print($Timer.time_left)
+	if event is InputEventMouseButton &&  event.button_index == BUTTON_LEFT and event.pressed :
+		self.queue_free()
+		 # Je supprime l'objet directement au lieu de juste le cacher comme en bas
+		#$Sprite.visible=false
